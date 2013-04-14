8947fe81b9ca844fa655edaa00454dec49082b9c
terryngo <terry.c.ngo@gmail.com>
2013-04-14 05:17:34 -0700

    Update mangareaderlinkreplacer.user.js

--- mangareaderlinkreplacer.user.js
+++ mangareaderlinkreplacer.user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name           MangareaderLinkReplacer
 // @namespace      https://github.com/terryngo
-// @description    Replaces all mangareader links with mangapanda on all sites (unless you are on mangareader itself)
+// @description    Replaces all mangareader links with mangapanda on all sites (unless you are on mangareader itself).
 // @include        *
 // @version        1.0
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // ==/UserScript==
 //alert('Hello, World!');
 $(document).ready(function() {
-    $("a[href*='mangareader.net']").each(function() {
-        this.href = this.href.replace('mangareader.net', 'mangapanda.com');
+    var urlToReplace = 'mangareader.net';
+    var urlToReplaceWith = 'mangapanda.com';
+    $("a[href*=urlToReplace]").each(function() {
+        this.href = this.href.replace(urlToReplace, urlToReplaceWith);
     });
 }); //end ready
